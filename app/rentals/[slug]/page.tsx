844744df5473ce5a502049d9b4746feb719d4007
Phillibus <philmcelroy80@gmail.com>
2025-01-19 22:21:28 -0600

    refactor: navbar refactor

---
+++ app/rentals/[slug]/page.tsx
@@ -0,0 +1,9 @@
+const SlugPage = () => {
+    return (
+        <div className='text-3xl'>
+            Create Rental
+        </div>
+    );
+}
+
+export default SlugPage;
--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -10,29 +10,19 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {Moon, Sun} from "lucide-react";
 import {TbSunMoon} from "react-icons/tb";
+import {useEffect} from "react";
 
 const DarkMode = () => {
     const {theme, setTheme} = useTheme();
+
+    useEffect(() => setTheme('light'), [setTheme]);
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <Sun
-                        className={`h-[1.2rem] w-[1.2rem] transition-all ${
-                            theme === "light" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-                        }`}
-                    />
-                    <Moon
-                        className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
-                            theme === "dark" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-                        }`}
-                    />
-                    <TbSunMoon
-                        className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
-                            theme === "system" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-                        }`}
-                    />
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
